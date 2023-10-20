eb4a057f95807bc018369857c3a9397b58ca8b58
Shubham Matale <113490947+shubham562004@users.noreply.github.com>
2023-10-20 16:01:05 +0530

    Create Insertion.py

---
+++ Insertion.py
@@ -0,0 +1,14 @@
+# Sample list
+my_list = [1, 2, 3, 5, 6]
+
+# Element to insert
+element_to_insert = 4
+
+# Index at which to insert the element
+insert_index = 3
+
+# Insert the element at the specified index
+my_list.insert(insert_index, element_to_insert)
+
+# Print the updated list
+print(my_list)
